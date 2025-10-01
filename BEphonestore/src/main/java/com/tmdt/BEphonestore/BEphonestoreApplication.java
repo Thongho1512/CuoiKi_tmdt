6691cd464fcf7547e0d188d984468e504043855b
thongho <thongho1512@gmail.com>
2025-10-01 11:54:30 +0700

    add backend project

---
+++ BEphonestore/src/main/java/com/tmdt/BEphonestore/BEphonestoreApplication.java
@@ -0,0 +1,13 @@
+package com.tmdt.BEphonestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BEphonestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BEphonestoreApplication.class, args);
+	}
+
+}
---
+++ BEphonestore/src/test/java/com/tmdt/BEphonestore/BEphonestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmdt.BEphonestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BEphonestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
